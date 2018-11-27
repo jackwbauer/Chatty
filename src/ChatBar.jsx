138e17baeb3358154bd29f49352f2be72889a384
Jack Bauer <bauer.jackw@gmail.com>
2018-11-26 18:35:00 -0800

    passes currentUser to ChatBar

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -4,7 +4,7 @@ class ChatBar extends Component {
     render() {
         return (
             <footer className="chatbar">
-                <input className="chatbar-username" placeholder="Your Name (Optional)" />
+                <input className="chatbar-username" value={this.props.currentUser.name ? this.props.currentUser.name : ''} placeholder="Your Name (Optional)" />
                 <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
             </footer>
         )
